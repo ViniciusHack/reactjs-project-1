8d7db9d7b5a2f10d7bae94e46a26d03ac3d6b629
Vinícius Hack <viniciusvwh1441@gmail.com>
2021-09-09 05:58:19 -0300

    '==='

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/global-styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global-styles.css';
 // import App from "./App";
-import { Home } from "./templates/Home";
+import { Home } from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/templates/Home/Home.spec.jsx
@@ -0,0 +1,4 @@
+// it e test tem mesma fução.
+it('dummy test', () => {
+  expect(1).toBe(1);
+});
